eb6affbb09f8ef7047af1c0e9240edaae38bc0b0
waldeci freitas <freitas.wal18@gmail.com>
2022-09-21 21:01:24 -0300

    insert method created

--- modules/tree.py
+++ modules/tree.py
@@ -26,34 +26,66 @@ class PatriciaTree:
                 if IS_LEAF:
                     if node.value == word:
                         return False
-                    else:           
+                    else:
 
                         nodeValue = node.value
                         index = helper.getMismatchIndex(nodeValue, word)
+
                         nodeChar = helper.getCharAtIndex(nodeValue, index)
-                        wordChar = helper.getCharAtIndex(word, index) 
+                        wordChar = helper.getCharAtIndex(word, index)
+                        char = helper.getSmaller(nodeChar, wordChar)
 
                         IS_ROOT = node.ancestor == None
 
                         if IS_ROOT:
-                            char = helper.getSmaller(nodeChar, wordChar)
                             self.root = InternNode(index, char)
                             node = self.root
+
+                            if nodeChar > wordChar:
+                                node.leftChild = Leaf(word)
+                                node.rightChild = Leaf(nodeValue)
+
+                            if nodeChar < wordChar:
+                                node.leftChild = Leaf(nodeValue)
+                                node.rightChild = Leaf(word)
+
+                            node.rightChild.ancestor = node
+                            node.leftChild.ancestor = node
+
+                            return True
+
                         else:
-                            print('it is not root')
-                        if nodeChar > wordChar:
-                            node.leftChild = Leaf(word)
-                            node.rightChild = Leaf(nodeValue)
-       
-                        
-                        if nodeChar < wordChar:
-                            node.leftChild = Leaf(nodeValue)    
-                            node.rightChild = Leaf(word)
-                        
-                        node.rightChild.ancestor = node
-                        node.leftChild.ancestor = node
-                        
-                        return True                            
+                            IS_LEFT_CHILD = node.ancestor.leftChild.isEquals(node)
+                  
+                            if IS_LEFT_CHILD:
+                                
+                                node.ancestor.leftChild = InternNode(index, char)
+
+                                if nodeChar > wordChar:
+                                    node.ancestor.leftChild.leftChild = Leaf(word)
+                                    node.ancestor.leftChild.rightChild = Leaf(nodeValue)
+
+                                if nodeChar < wordChar:
+                                    node.ancestor.leftChild.leftChild = Leaf(nodeValue)
+                                    node.ancestor.leftChild.rightChild = Leaf(word)
+                                  
+                                node.ancestor.leftChild.leftChild.ancestor = node.ancestor.leftChild
+                                node.ancestor.leftChild.rightChild.ancestor = node.ancestor.leftChild
+                            else:
+                                node.ancestor.rightChild = InternNode(index, char)
+
+                                if nodeChar > wordChar:
+                                    node.ancestor.rightChild.leftChild = Leaf(word)
+                                    node.ancestor.rightChild.rightChild = Leaf(nodeValue)
+
+                                if nodeChar < wordChar:
+                                    node.ancestor.rightChild.leftChild = Leaf(nodeValue)
+                                    node.ancestor.rightChild.rightChild = Leaf(word)
+                                  
+                                node.ancestor.rightChild.leftChild.ancestor = node.ancestor.leftChild
+                                node.ancestor.rightChild.rightChild.ancestor = node.ancestor.leftChild
+
+                            break
                 else:
                     nodeChar = node.dismatchedChar
 
@@ -65,27 +97,27 @@ class PatriciaTree:
                     else:
                         node = node.leftChild
 
-
     def search(self, word):
         pass
 
     def remove(self, word):
         pass
 
-    
     def print(self):
         if self.root == None:
             print('Arvore Vazia')
         else:
-            self._printTree(self.root, 'Root' )
-
+            self._printTree(self.root, 'Root')
 
     def _printTree(self, node: Node, subtree):
         if Node is not None:
-            if isinstance(node, Leaf):
-                print(f'node -> {node.value} | subtree: {subtree} | Ancestor: {node.ancestor.dismatchedChar}')
-            
-            elif isinstance(node, InternNode):
-                self._printTree(node.leftChild, 'left')
-                self._printTree(node.rightChild, 'right')
 
+            IS_LEAF = isinstance(node, Leaf)
+
+            if IS_LEAF:
+                print(
+                    f'Node -> {node.value} | Dismatch: {node.ancestor.dismatchedChar} | Path: {subtree} ')
+
+            elif isinstance(node, InternNode):
+                self._printTree(node.leftChild, subtree + ' -> left')
+                self._printTree(node.rightChild, subtree + ' -> right')
